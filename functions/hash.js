50f249f04a3320e53ebb0dec84e11aba145ceac7
Michael Robinson <mike@pagesofinterest.net>
2021-09-19 19:11:20 +1200

    Changed hash state to use JSON instead of more brittle custom format

--- functions/hash.js
+++ functions/hash.js
@@ -1,33 +1,31 @@
-import { getTrackIds } from './track'
+export const hashToState = (defaultState, hash, tracks) => {
+  if (!hash) {
+    return null
+  }
 
-const coerceMilestone = (value) => {
-  // HACK I know this is goofy but i'm dealing with flow typing
-  switch (value) {
-    case 0: return 0
-    case 1: return 1
-    case 2: return 2
-    case 3: return 3
-    case 4: return 4
-    case 5: return 5
-    default: return 0
+  let decodedState = null
+
+  try {
+    decodedState = JSON.parse(decodeURI(hash.replace(/^#/, '')))
+  } catch (e) {
+    return defaultState
+  }
+
+  return {
+    ...defaultState,
+    ...decodedState
   }
 }
 
-export const hashToState = (defaultState, hash, tracks) => {
-  if (!hash) return null
-  const result = defaultState
-  const hashValues = hash.split('#')[1].split(',')
-  if (!hashValues) return null
-  getTrackIds(tracks).forEach((trackId, i) => {
-    result.milestoneByTrack[trackId] = coerceMilestone(Number(hashValues[i]))
-  })
-  if (hashValues[16]) result.name = decodeURI(hashValues[16])
-  if (hashValues[17]) result.title = decodeURI(hashValues[17])
-  return result
-}
+export const stateToHash = (state) => {
+  if (!state || !state.milestoneByTrack) {
+    return null
+  }
 
-export const stateToHash = (state, tracks) => {
-  if (!state || !state.milestoneByTrack) return null
-  const values = getTrackIds(tracks).map(trackId => state.milestoneByTrack[trackId]).concat(encodeURI(state.name), encodeURI(state.title))
-  return values.join(',')
+  const hashValues = {
+    name: state.name,
+    title: state.title,
+    milestoneByTrack: state.milestoneByTrack
+  }
+  return encodeURI(JSON.stringify(hashValues))
 }
